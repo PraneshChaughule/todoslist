5b745ee12995c3e7f180e0478a6eb98303f4907f
pran22 <praneshchaughule23@gmail.com>
2022-04-09 10:01:39 +0530

    all changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import './App.css';
+import Header from "./MyComponent/Header";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+// const Todos = lazy(() => import('./MyComponent/Todos'))
+// const AddTodo = lazy(() => import('./MyComponent/AddTodo'))
+// const Footer = lazy(() => import('./MyComponent/Footer'))
+// const About = lazy(() => import('./MyComponent/About'))
+import {Todos} from "./MyComponent/Todos";
+import {AddTodo} from "./MyComponent/AddTodo";
+import {Footer} from "./MyComponent/Footer";
+import {About} from "./MyComponent/About";
+
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  const addTodo = (title, desc) => {
+    console.log("I am reading this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+
+  }
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title="My Todos List" />
+          <Switch>
+            <Route exact path="/" render={() => {
+              return (
+                <>
+                  <AddTodo addTodo={addTodo} />
+                  <Todos todos={todos} onDelete={onDelete} />
+                </>)
+            }}>
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+
+
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/MyComponent/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This an about componet.
+            <p>React lets you define components as classes or functions. Components defined as classes currently provide more features which are described in detail on this page. To define a React component class, you need to extend</p>
+        </div>
+    )
+}
